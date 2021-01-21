16586be77b648a74c341e013cdb3efe50b926bcb
duplxey <nick.tomazic@gmail.com>
2021-01-21 22:01:16 +0100

    blog app, Article, ninja API, ninja router

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Article(models.Model):
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=512, unique=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.author.username + ": " + self.title
--- django_with_ninja/urls.py
+++ django_with_ninja/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+from blog.api import router as blog_router
+
+api = NinjaAPI()
+api.add_router("/blog/", blog_router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", api.urls),
 ]
