019c66d153c06218b7be148aa63f683d4e6a86db
Michael Herman <hermanmu@gmail.com>
2021-01-28 20:56:51 -0600

    updates

--- blog/api.py
+++ blog/api.py
@@ -10,25 +10,13 @@ from blog.schemas import ArticleOut, ArticleIn
 router = Router()
 
 
-@router.get("/article/{article_id}", response=ArticleOut)
-def get_article(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
-    return article
-
-
-@router.get("/article-list", response=List[ArticleOut])
-def get_articles(request):
-    articles = Article.objects.all()
-    return articles
-
-
-@router.post("/create-article")
+@router.post("/articles/create")
 def create_article(request, payload: ArticleIn):
-    passed = payload.dict()
+    data = payload.dict()
     try:
-        author = User.objects.get(id=passed['author'])
-        del passed['author']
-        article = Article.objects.create(author=author, **passed)
+        author = User.objects.get(id=data['author'])
+        del data['author']
+        article = Article.objects.create(author=author, **data)
         return {
             'detail': "Article has been successfully created.",
             'id': article.id,
@@ -36,3 +24,14 @@ def create_article(request, payload: ArticleIn):
     except User.DoesNotExist:
         return {'detail': "The specific user cannot be found."}
 
+
+@router.get("/articles/{article_id}", response=ArticleOut)
+def get_article(request, article_id: int):
+    article = get_object_or_404(Article, id=article_id)
+    return article
+
+
+@router.get("/articles", response=List[ArticleOut])
+def get_articles(request):
+    articles = Article.objects.all()
+    return articles
